d54acd2df3b1fba4146eaa9093aa88b9f9b3cb76
Maël Nison <nison.mael@gmail.com>
2025-10-14 20:50:08 +0200

    Enables CONFIG_UEVENT_HELPER

--- post-build.sh
+++ post-build.sh
@@ -6,6 +6,8 @@ TARGET_FILESYSTEM="$1"
 
 cd $TARGET_FILESYSTEM
 
+chmod +x etc/init.sh
+
 if [[ -e "$BUILDROOT_TREE/post-build-user.sh" ]]; then
     "$BUILDROOT_TREE/post-build-user.sh"
 fi
--- rootfs-overlay/etc/init.sh
+++ rootfs-overlay/etc/init.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 echo /sbin/mdev > /proc/sys/kernel/hotplug
-touch /dev/mdev.log
+mdev -s
