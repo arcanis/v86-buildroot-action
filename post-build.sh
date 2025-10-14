532336201e8669b6fcc3f1cdcc6d4dfcdb0cbfae
Maël Nison <nison.mael@gmail.com>
2025-10-15 01:16:21 +0200

    Switches to auto-cd initialization

--- post-build.sh
+++ post-build.sh
@@ -7,7 +7,6 @@ TARGET_FILESYSTEM="$1"
 cd $TARGET_FILESYSTEM
 
 chmod +x etc/init.d/S90v86
-chmod +x lib/automount.sh
 
 if [[ -e "$BUILDROOT_TREE/post-build-user.sh" ]]; then
     "$BUILDROOT_TREE/post-build-user.sh"
